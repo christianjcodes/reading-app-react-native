375d4efb47ca940050692e4bbb738ae7fe3991f1
Christian Johnson <cartian4@gmail.com>
2024-08-09 06:32:38 -0400

    Add components and app directories to tailwind config.js, fix tailwind error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./assets/**/*.{js,jsx,ts,tsx}"],
+  content: ["./components.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },

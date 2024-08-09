0482ddc71170d800149a05bbc2ba3ea0b739c8b2
Christian Johnson <cartian4@gmail.com>
2024-08-09 05:58:16 -0400

    Add profile page and a link to it from the homepage

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { Link } from 'expo-router';
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Reading App!</Text>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+    		<Text>Reading App!</Text>
+			<StatusBar style="auto" />
+			<Link href="/profile" style={{ color: 'blue' }}>
+				Go to Profile</Link>
+		</View>
+	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});

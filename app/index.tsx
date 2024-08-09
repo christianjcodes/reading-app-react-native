c746a902e1e44e7e80da9d5c827da26934ad6f17
Christian Johnson <cartian4@gmail.com>
2024-08-09 06:23:30 -0400

    Fix tailwindcss error

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { StatusBar } from 'expo-status-bar';
 import { Link } from 'expo-router';
 
 export default function Index() {
 	return (
-		<View style={styles.container}>
+		<View className="flex-1 items-center justify-center bg-white">
     		<Text>Reading App!</Text>
 			<StatusBar style="auto" />
 			<Link href="/profile" style={{ color: 'blue' }}>
@@ -12,12 +12,3 @@ export default function Index() {
 		</View>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});

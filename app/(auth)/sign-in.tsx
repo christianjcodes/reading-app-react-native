714bfb586d11c41fc20881c8593a8909203460f6
Christian Johnson <cartian4@gmail.com>
2024-08-19 05:54:15 -0400

    Add sign in button to sign in page

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -4,6 +4,8 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../../constants';
 import FormField from '../../components/FormField';
 import { useState } from 'react';
+import { CustomButton } from '../../components';
+import { Link } from 'expo-router';
 
 const SignIn = () => {
 
@@ -11,6 +13,12 @@ const SignIn = () => {
 		email: '',
 		password: ''
 	})
+
+	const [isSubmitting, setIsSubmitting] = useState(false)
+
+	const submit = () => {
+
+	}
 
 	return (
 		<SafeAreaView className='bg-primary h-full'>
@@ -37,6 +45,24 @@ const SignIn = () => {
 						password: e})}
 						otherStyles="mt-7"
 					/>
+
+					<CustomButton 
+						title="Sign In"
+						handlePress={submit}
+						containerStyles="mt-7"
+						isLoading={isSubmitting}
+					/>
+
+					<View className='justify-center pt-5 flex-row gap-2'>
+						<Text className='text-lg text-gray-100
+						font-pregular'>
+							Don't have an account?
+						</Text>
+						<Link href="/sign-up" className='text-lg
+						font-psemibold text-secondary'>
+							Sign Up
+						</Link>	
+					</View>
 				</View>
 			</ScrollView>
 		</SafeAreaView>
